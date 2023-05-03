f22bd38516a781a50d60d1dcf817c5c684a517fb
Ricardonovais1 <ricardonovais1@gmail.com>
2023-05-03 08:02:22 -0300

    Resolvendo o princípio do design da app

--- spec/system/user_view_homepage_spec.rb
+++ spec/system/user_view_homepage_spec.rb
@@ -22,16 +22,18 @@ describe 'Usuário visita a página inicial' do
     
     # Assert
     expect(page).to have_content 'Lote AAA000000'
-    expect(page).to have_content "Data de início: 2090-10-10" 
-    expect(page).to have_content "Data limite para lances: 2090-10-20" 
-    expect(page).to have_content "Valor mínimo para lance: R$100,00"
-    expect(page).to have_content "Menor diferença permitida entre lances: R$10,00"
+    expect(page).to have_content "2090-10-10 a 2090-10-20" 
+    # expect(page).to have_content "Data limite para lances: 2090-10-20" 
+    # expect(page).to have_content "Valor mínimo para lance: R$100,00"
+    # expect(page).to have_content "Menor diferença permitida entre lances: R$10,00"
 
     expect(page).to have_content 'Lote BBB111111'
-    expect(page).to have_content "Data de início: 2090-10-20" 
-    expect(page).to have_content "Data limite para lances: 2090-10-30" 
-    expect(page).to have_content "Valor mínimo para lance: R$200,00"
-    expect(page).to have_content "Menor diferença permitida entre lances: R$20,00"
+    expect(page).to have_content "2090-10-20 a 2090-10-30" 
+
+    # expect(page).to have_content "Data de início: 2090-10-20" 
+    # expect(page).to have_content "Data limite para lances: 2090-10-30" 
+    # expect(page).to have_content "Valor mínimo para lance: R$200,00"
+    # expect(page).to have_content "Menor diferença permitida entre lances: R$20,00"
   end
 
   it 'e não há lotes cadastrados' do 
