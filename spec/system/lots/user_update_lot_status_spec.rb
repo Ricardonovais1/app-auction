90a650a41ea0f0fcc2db1722da157804daf37633
Ricardonovais1 <ricardonovais1@gmail.com>
2023-05-11 08:33:27 -0300

    Somente outro admin pode aprovar lote

--- spec/system/lots/user_update_lot_status_spec.rb
+++ spec/system/lots/user_update_lot_status_spec.rb
@@ -77,4 +77,25 @@ describe 'Admin muda status do lote para ativo' do
     expect(page).to have_button 'Aprovar lote'
     expect(page).not_to have_button 'Marcar pendente'
   end
+
+  it 'e só outro usuário admin pode aprovar o lote' do 
+    # Arrange 
+    admin_1 = User.create!(name: 'Ricardo', email: 'ricardo@leilaodogalpao.com.br', registration_number: '70535073607', password: 'password')
+    admin_2 = User.create!(name: 'Ana', email: 'ana@leilaodogalpao.com.br', registration_number: '98512692049', password: 'password')
+    lot = Lot.create!(code: 'ABC123987', start_date: '2090-10-20', limit_date: '2090-10-30', 
+                      minimum_bid_value: 100, minimum_bid_difference: 10, by: 'Ricardo', by_email: 'ricardo@leilaodogalpao.com.br') 
+
+    # Act 
+    login_as(admin_1)
+    visit root_path
+    within('nav') do 
+      click_on 'Administrativo'
+      click_on 'Lotes pendentes'
+    end
+    click_on 'ABC123987'
+
+    # Assert
+    expect(page).not_to have_button 'Aprovar lote'
+    expect(page).to have_content 'Situação do lote: Aguardando aprovação'
+  end
 end
